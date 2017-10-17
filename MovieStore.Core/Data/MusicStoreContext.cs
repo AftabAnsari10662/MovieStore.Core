e2e09a0246d40dc35329f23c907ebfbcdcece702
Aftab Ansari <afansari@itron.com>
2017-10-17 09:54:36 +0000

    Initial commit

---
+++ MovieStore.Core/Data/MusicStoreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieStore.Core.Data
+{
+    public class MusicStoreContext : DbContext
+    {
+        public MusicStoreContext(
+            DbContextOptions<MusicStoreContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int RelaseYear { get; set; }
+    }
+
+}
